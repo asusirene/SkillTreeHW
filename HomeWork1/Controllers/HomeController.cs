592cf0a3a69eaa37170dc8787704979deb56e170
小幼 何 <ily1872@hotmail.com>
2019-12-06 19:18:31 +0800

    完成帳本列表

--- HomeWork1/Controllers/HomeController.cs
+++ HomeWork1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeWork1.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace HomeWork1.Controllers
     {
         public ActionResult Index()
         {
+
             return View();
         }
 
@@ -25,6 +27,25 @@ namespace HomeWork1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Account()
+        {
+            string[] categoryArray = new string[] { "收入", "支出" };
+            //塞入資料
+            Random rnd = new Random();
+            List<AccountViewModel> accountModelList = new List<AccountViewModel>();
+            for (int i = 0; i <= 100; i++)
+            {
+                AccountViewModel accountModel = new AccountViewModel();
+                accountModel.Idx = i;
+                accountModel.Category = categoryArray[rnd.Next(0, 2)];
+                accountModel.LogDate = DateTime.Now.AddDays(rnd.Next(99));
+                accountModel.Money = rnd.Next(1, 9999);
+                accountModelList.Add(accountModel);
+            }
+            return View(accountModelList);
         }
     }
 }
---
+++ HomeWork1/Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork1.Models.ViewModels
+{
+    public class AccountViewModel
+    {
+        public int Idx { get; set; }
+        public string Category { get; set; }
+        public DateTime LogDate { get; set; }
+        public int Money { get; set; }
+    }
+}
